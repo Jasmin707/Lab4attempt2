ec45feae4f03ff1111b543942953a21b3f00bd06
Ethanicuss <ethancranmer@utexas.edu>
2016-11-08 15:57:59 -0600

    added all the headers

--- Lab4/src/assignment5/Encounters.java
+++ Lab4/src/assignment5/Encounters.java
@@ -1,4 +1,15 @@
-package assignment5;
+/* CRITTERS Encounter.java
+ * EE422C Project 5 submission by
+ * Jasmin Rajan
+ * jor427
+ * 16470
+ * Ethan Cranmer
+ * elc2255
+ * 16475
+ * Slip days used: <0>
+ * Git URL: https://github.com/Jasmin707/Lab4attempt2
+ * Fall 2016
+ */package assignment5;
 
 import java.util.ArrayList;
 
--- Lab4/src/assignment5/StatsWindow.java
+++ Lab4/src/assignment5/StatsWindow.java
@@ -1,3 +1,15 @@
+/* CRITTERS StatsWindow.java
+ * EE422C Project 5 submission by
+ * Jasmin Rajan
+ * jor427
+ * 16470
+ * Ethan Cranmer
+ * elc2255
+ * 16475
+ * Slip days used: <0>
+ * Git URL: https://github.com/Jasmin707/Lab4attempt2
+ * Fall 2016
+ */
 package assignment5;
 
 import javafx.geometry.Insets;
